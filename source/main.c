f6bf0babbbd5fe2fe86f5af29cee5017e97824c7
Brett McCausland <brettmccausland@gmail.com>
2020-03-08 14:26:25 -0700

    Usart Blocking

--- source/main.c
+++ source/main.c
@@ -100,7 +100,7 @@ int BluetoothTick(int state){
      break;
 
    case OFF_BLT:
-      state = ((Data_in -'0') =='1') ? ON_BLT:OFF_BLT;
+      state = (Data_in  =='1') ? ON_BLT:OFF_BLT;
      break;
 
    }
@@ -110,18 +110,6 @@ int BluetoothTick(int state){
        break;
 
      case OFF_BLT:
-       if((Data_in -'0') < '1'){
-         nokia_lcd_write_string("less 1",1);
-         nokia_lcd_set_cursor(0, 10);
-         nokia_lcd_write_string(Data_in, 1);
-         nokia_lcd_render();
-       }
-       if((Data_in -'0') > '1'){
-         nokia_lcd_write_string("greater 1",1);
-         nokia_lcd_set_cursor(0, 10);
-         nokia_lcd_write_string(Data_in, 1);
-         nokia_lcd_render();
-       }
        if(Data_in == '0'){
          nokia_lcd_write_string("equal 0",1);
          nokia_lcd_set_cursor(0, 10);
